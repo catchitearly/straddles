db7cd120a6b7387deb42d4375f95999b2a5aa1be
viraltrend <catchitearly.viraltrend@gmail.com>
2026-05-06 07:20:58 +0530

    Update straddle_analyzer.py

--- straddle_analyzer.py
+++ straddle_analyzer.py
@@ -14,7 +14,7 @@ import sys
 CLIENT_ID = os.getenv("CLIENT_ID")
 TOKEN = os.getenv("FYERS_ACCESS_TOKEN")
 TARGET_DATE = datetime.now(ZoneInfo("Asia/Kolkata")).strftime("%Y-%m-%d")
-EXPIRY = "26505"  # Update as needed
+EXPIRY = "26512"  # Update as needed
 OFFSETS = [-400, -300, -200, -100, 0, 100, 200, 300, 400]
 
 OUTPUT_DIR = "output"
